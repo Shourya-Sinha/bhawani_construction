956152de12674212302eb40b6ad34ce8e19c7a41
SHOURYA-SINHA <jmditolima@gmail.com>
2025-05-24 13:35:44 +0700

    Prepare for GitHub Pages deploy after update app.tsx and verifying base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
+
+const repoName = "bhawani_construction";
 export default defineConfig(({ mode }) => ({
+  base: '/bhawani_construction/',
   server: {
     host: "::",
     port: 8080,
@@ -19,4 +22,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+    build: {
+    outDir: 'dist',
+  },
+  // build: {
+  //   outDir: path.resolve(__dirname, 'dist'),
+  // },
 }));
